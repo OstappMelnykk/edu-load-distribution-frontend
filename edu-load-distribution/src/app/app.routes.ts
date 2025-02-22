b8f7ac875c9d26c08c8ca7e26b35e80b8b32d3a7
OstappMelnykk <79ostap@ukr.net>
2025-02-22 21:41:09 +0200

    feat(routing): configure application routes with authGuard protection

--- edu-load-distribution/src/app/app.routes.ts
+++ edu-load-distribution/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './modules/auth/login/login.component';
+import {RegisterComponent} from './modules/auth/register/register.component';
+import {SubjectListComponent} from './modules/subjects/subject-list/subject-list.component';
+import {TeacherListComponent} from './modules/teachers/teacher-list/teacher-list.component';
+import {WorkloadListComponent} from './modules/workloads/workload-list/workload-list.component';
+import {DashboardComponent} from './modules/dashboard/dashboard/dashboard.component';
+import {authGuard} from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard] // Захист маршруту
+  },
+  {
+    path: 'subjects',
+    component: SubjectListComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'teachers',
+    component: TeacherListComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'workloads',
+    component: WorkloadListComponent,
+    canActivate: [authGuard]
+  }
+];
