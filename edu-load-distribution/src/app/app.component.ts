b817ea72c10e44441a5ae84998c0bc1cd8894ebe
OstappMelnykk <79ostap@ukr.net>
2025-02-23 23:31:01 +0200

    feat: enhance structure and design of the app component

--- edu-load-distribution/src/app/app.component.ts
+++ edu-load-distribution/src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavbarComponent} from './shared/navbar/navbar.component';
 import {AuthService} from './core/services/auth.service';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
+import {CommonModule, NgIf} from '@angular/common';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, NavbarComponent],
+    imports: [RouterOutlet, NavbarComponent, CommonModule],
     templateUrl: 'app.component.html',
     styleUrl: 'app.component.scss'
 })
